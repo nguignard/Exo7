25f7ccd5386de9167272a4ce765ac4852472acbe
nguignard <nguignard@gmail.com>
2017-03-20 10:15:07 +0100

    Creation Classe Donnes

--- Exo6/frmStagiaire.Designer.cs
+++ Exo6/frmStagiaire.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.grpStagiaire = new System.Windows.Forms.GroupBox();
-            this.lblOsia = new System.Windows.Forms.Label();
-            this.txtOsia = new System.Windows.Forms.TextBox();
-            this.txtCP = new System.Windows.Forms.TextBox();
-            this.lblCP = new System.Windows.Forms.Label();
-            this.txtNom = new System.Windows.Forms.TextBox();
-            this.lblNom = new System.Windows.Forms.Label();
-            this.txtPrenom = new System.Windows.Forms.TextBox();
-            this.lblPrenom = new System.Windows.Forms.Label();
-            this.txtAdresse = new System.Windows.Forms.TextBox();
-            this.lblAdresse = new System.Windows.Forms.Label();
             this.txtVille = new System.Windows.Forms.TextBox();
             this.lalblVillebel6 = new System.Windows.Forms.Label();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.lblAdresse = new System.Windows.Forms.Label();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.lblPrenom = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.txtCP = new System.Windows.Forms.TextBox();
+            this.lblCP = new System.Windows.Forms.Label();
+            this.txtOsia = new System.Windows.Forms.TextBox();
+            this.lblOsia = new System.Windows.Forms.Label();
             this.grpStagiaire.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,69 +65,21 @@
             this.grpStagiaire.TabStop = false;
             this.grpStagiaire.Text = "Stagiaire";
             // 
-            // lblOsia
+            // txtVille
             // 
-            this.lblOsia.AutoSize = true;
-            this.lblOsia.Location = new System.Drawing.Point(7, 20);
-            this.lblOsia.Name = "lblOsia";
-            this.lblOsia.Size = new System.Drawing.Size(81, 13);
-            this.lblOsia.TabIndex = 0;
-            this.lblOsia.Text = "Numéro d\'Osias";
+            this.txtVille.Location = new System.Drawing.Point(114, 121);
+            this.txtVille.Name = "txtVille";
+            this.txtVille.Size = new System.Drawing.Size(278, 20);
+            this.txtVille.TabIndex = 11;
             // 
-            // txtOsia
+            // lalblVillebel6
             // 
-            this.txtOsia.Location = new System.Drawing.Point(114, 17);
-            this.txtOsia.Name = "txtOsia";
-            this.txtOsia.Size = new System.Drawing.Size(100, 20);
-            this.txtOsia.TabIndex = 1;
-            // 
-            // txtCP
-            // 
-            this.txtCP.Location = new System.Drawing.Point(114, 147);
-            this.txtCP.Name = "txtCP";
-            this.txtCP.Size = new System.Drawing.Size(100, 20);
-            this.txtCP.TabIndex = 3;
-            // 
-            // lblCP
-            // 
-            this.lblCP.AutoSize = true;
-            this.lblCP.Location = new System.Drawing.Point(7, 150);
-            this.lblCP.Name = "lblCP";
-            this.lblCP.Size = new System.Drawing.Size(35, 13);
-            this.lblCP.TabIndex = 2;
-            this.lblCP.Text = "label2";
-            // 
-            // txtNom
-            // 
-            this.txtNom.Location = new System.Drawing.Point(114, 43);
-            this.txtNom.Name = "txtNom";
-            this.txtNom.Size = new System.Drawing.Size(278, 20);
-            this.txtNom.TabIndex = 5;
-            // 
-            // lblNom
-            // 
-            this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(7, 46);
-            this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(29, 13);
-            this.lblNom.TabIndex = 4;
-            this.lblNom.Text = "Nom";
-            // 
-            // txtPrenom
-            // 
-            this.txtPrenom.Location = new System.Drawing.Point(114, 69);
-            this.txtPrenom.Name = "txtPrenom";
-            this.txtPrenom.Size = new System.Drawing.Size(278, 20);
-            this.txtPrenom.TabIndex = 7;
-            // 
-            // lblPrenom
-            // 
-            this.lblPrenom.AutoSize = true;
-            this.lblPrenom.Location = new System.Drawing.Point(7, 72);
-            this.lblPrenom.Name = "lblPrenom";
-            this.lblPrenom.Size = new System.Drawing.Size(43, 13);
-            this.lblPrenom.TabIndex = 6;
-            this.lblPrenom.Text = "Prénom";
+            this.lalblVillebel6.AutoSize = true;
+            this.lalblVillebel6.Location = new System.Drawing.Point(7, 124);
+            this.lalblVillebel6.Name = "lalblVillebel6";
+            this.lalblVillebel6.Size = new System.Drawing.Size(54, 13);
+            this.lalblVillebel6.TabIndex = 10;
+            this.lalblVillebel6.Text = "labeVillel6";
             // 
             // txtAdresse
             // 
@@ -145,21 +97,69 @@
             this.lblAdresse.TabIndex = 8;
             this.lblAdresse.Text = "Adresse";
             // 
-            // txtVille
+            // txtPrenom
             // 
-            this.txtVille.Location = new System.Drawing.Point(114, 121);
-            this.txtVille.Name = "txtVille";
-            this.txtVille.Size = new System.Drawing.Size(278, 20);
-            this.txtVille.TabIndex = 11;
+            this.txtPrenom.Location = new System.Drawing.Point(114, 69);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(278, 20);
+            this.txtPrenom.TabIndex = 7;
             // 
-            // lalblVillebel6
+            // lblPrenom
             // 
-            this.lalblVillebel6.AutoSize = true;
-            this.lalblVillebel6.Location = new System.Drawing.Point(7, 124);
-            this.lalblVillebel6.Name = "lalblVillebel6";
-            this.lalblVillebel6.Size = new System.Drawing.Size(54, 13);
-            this.lalblVillebel6.TabIndex = 10;
-            this.lalblVillebel6.Text = "labeVillel6";
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(7, 72);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(43, 13);
+            this.lblPrenom.TabIndex = 6;
+            this.lblPrenom.Text = "Prénom";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(114, 43);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(278, 20);
+            this.txtNom.TabIndex = 5;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(7, 46);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(29, 13);
+            this.lblNom.TabIndex = 4;
+            this.lblNom.Text = "Nom";
+            // 
+            // txtCP
+            // 
+            this.txtCP.Location = new System.Drawing.Point(114, 147);
+            this.txtCP.Name = "txtCP";
+            this.txtCP.Size = new System.Drawing.Size(100, 20);
+            this.txtCP.TabIndex = 3;
+            // 
+            // lblCP
+            // 
+            this.lblCP.AutoSize = true;
+            this.lblCP.Location = new System.Drawing.Point(7, 150);
+            this.lblCP.Name = "lblCP";
+            this.lblCP.Size = new System.Drawing.Size(64, 13);
+            this.lblCP.TabIndex = 2;
+            this.lblCP.Text = "Code Postal";
+            // 
+            // txtOsia
+            // 
+            this.txtOsia.Location = new System.Drawing.Point(114, 17);
+            this.txtOsia.Name = "txtOsia";
+            this.txtOsia.Size = new System.Drawing.Size(100, 20);
+            this.txtOsia.TabIndex = 1;
+            // 
+            // lblOsia
+            // 
+            this.lblOsia.AutoSize = true;
+            this.lblOsia.Location = new System.Drawing.Point(7, 20);
+            this.lblOsia.Name = "lblOsia";
+            this.lblOsia.Size = new System.Drawing.Size(81, 13);
+            this.lblOsia.TabIndex = 0;
+            this.lblOsia.Text = "Numéro d\'Osias";
             // 
             // frmStagiaire
             // 
@@ -177,19 +177,19 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox grpStagiaire;
-        private System.Windows.Forms.TextBox txtVille;
-        private System.Windows.Forms.Label lalblVillebel6;
-        private System.Windows.Forms.TextBox txtAdresse;
-        private System.Windows.Forms.Label lblAdresse;
-        private System.Windows.Forms.TextBox txtPrenom;
-        private System.Windows.Forms.Label lblPrenom;
-        private System.Windows.Forms.TextBox txtNom;
-        private System.Windows.Forms.Label lblNom;
-        private System.Windows.Forms.TextBox txtCP;
-        private System.Windows.Forms.Label lblCP;
-        private System.Windows.Forms.TextBox txtOsia;
-        private System.Windows.Forms.Label lblOsia;
+        protected System.Windows.Forms.GroupBox grpStagiaire;
+        protected System.Windows.Forms.TextBox txtVille;
+        protected System.Windows.Forms.Label lalblVillebel6;
+        protected System.Windows.Forms.TextBox txtAdresse;
+        protected System.Windows.Forms.Label lblAdresse;
+        protected System.Windows.Forms.TextBox txtPrenom;
+        protected System.Windows.Forms.Label lblPrenom;
+        protected System.Windows.Forms.TextBox txtNom;
+        protected System.Windows.Forms.Label lblNom;
+        protected System.Windows.Forms.TextBox txtCP;
+        protected System.Windows.Forms.Label lblCP;
+        protected System.Windows.Forms.TextBox txtOsia;
+        protected System.Windows.Forms.Label lblOsia;
     }
 }
 
