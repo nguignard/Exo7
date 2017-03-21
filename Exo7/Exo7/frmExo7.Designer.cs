3ea5d1a3da95c0602d362b1eb36164866dc0c8c1
nguignard <nguignard@gmail.com>
2017-03-21 10:34:47 +0100

    frmExo7 - Design

--- Exo7/Exo7/frmExo7.Designer.cs
+++ Exo7/Exo7/frmExo7.Designer.cs
@@ -28,11 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblNomRecherche = new System.Windows.Forms.Label();
+            this.txtRecherche = new System.Windows.Forms.TextBox();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.btnTous = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(634, 253);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lblNomRecherche
+            // 
+            this.lblNomRecherche.AutoSize = true;
+            this.lblNomRecherche.Location = new System.Drawing.Point(26, 13);
+            this.lblNomRecherche.Name = "lblNomRecherche";
+            this.lblNomRecherche.Size = new System.Drawing.Size(85, 13);
+            this.lblNomRecherche.TabIndex = 1;
+            this.lblNomRecherche.Text = "Nom Recherche";
+            // 
+            // txtRecherche
+            // 
+            this.txtRecherche.Location = new System.Drawing.Point(116, 10);
+            this.txtRecherche.Name = "txtRecherche";
+            this.txtRecherche.Size = new System.Drawing.Size(382, 20);
+            this.txtRecherche.TabIndex = 2;
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.Location = new System.Drawing.Point(504, 8);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(75, 23);
+            this.btnRechercher.TabIndex = 3;
+            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = true;
+            // 
+            // btnTous
+            // 
+            this.btnTous.Location = new System.Drawing.Point(585, 8);
+            this.btnTous.Name = "btnTous";
+            this.btnTous.Size = new System.Drawing.Size(75, 23);
+            this.btnTous.TabIndex = 4;
+            this.btnTous.Text = "Tous";
+            this.btnTous.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(423, 336);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 5;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(504, 336);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 6;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuitter.Location = new System.Drawing.Point(585, 336);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 7;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            // 
+            // frmExo7
+            // 
+            this.AcceptButton = this.btnAjouter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnQuitter;
+            this.ClientSize = new System.Drawing.Size(673, 382);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnTous);
+            this.Controls.Add(this.btnRechercher);
+            this.Controls.Add(this.txtRecherche);
+            this.Controls.Add(this.lblNomRecherche);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmExo7";
             this.Text = "frmExo7";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblNomRecherche;
+        private System.Windows.Forms.TextBox txtRecherche;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.Button btnTous;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnQuitter;
     }
 }
--- Exo7/Exo7/frmExo7.cs
+++ Exo7/Exo7/frmExo7.cs
@@ -17,5 +17,10 @@ namespace Exo7
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
