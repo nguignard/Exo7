a7e43e68c1504f13ae1f54ff1516c01c0b747743
nguignard <nguignard@gmail.com>
2017-03-28 13:21:33 +0200

    the end

---
+++ Test/Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            public static int Calculer(int x) 
+            {
+                if (x >= 0)
+                    x = 1 - x;
+                else x = -x;
+
+                return 1 / x;
+            }
+        
+        }
+    }
+}
---
+++ Test/TestTests1/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CalculerTest()
+        {
+            int x = 2; // TODO : initialisez à une valeur appropriée 
+
+            int expected = 2;
+            int actual;
+
+            actual = Test.App3_ProgramAccessor.Calculer(x);
+
+            Assert.AreEqual(expected, actual, "App3.Program.Calculer n\'a pas retourné la valeur attendue.");
+            Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+    }
+}
